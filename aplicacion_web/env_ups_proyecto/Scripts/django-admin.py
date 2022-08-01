af43694999ba787f57e66c09614a490b0623350c
tarea-2-heroku <alexrey9725@gmail.com>
2022-07-31 21:00:28 -0500

    Agregando el codigo del Proyecto

---
+++ aplicacion_web/env_ups_proyecto/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\User\Pictures\proyecto_primer_parcial\aplicacion_web\env_ups_proyecto\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
